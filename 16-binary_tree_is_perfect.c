356466eea10db4db2806f66921afe6843a9dd97c
B1ueberry121 <2710@holbertonschool.com>
2021-07-23 01:35:00 +0000

    edge case

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,27 +1,46 @@
 #include "binary_trees.h"
 
-/**
- * height_count - counts the length of each branch of the tree
- * @tree: the tree to count
- * Return: the length
- */
 
-int height_count(const binary_tree_t *tree)
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: height of tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_subtree = 0;
-	int right_subtree = 0;
+	size_t left_subtree;
+	size_t right_subtree;
 
 	if (tree == NULL)
 		return (0);
 
-	left_subtree += height_count(tree->left) + 1;
-	right_subtree += height_count(tree->right) + 1;
+	left_subtree = binary_tree_height(tree->left);
+	right_subtree = binary_tree_height(tree->right);
 
 	if (left_subtree > right_subtree)
-	{
-		return (left_subtree);
-	}
-	return (right_subtree);
+		return (left_subtree + 1);
+	else
+		return (right_subtree + 1);
+}
+
+/**
+ * binary_tree_balance - measures the balance faactor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: 0 if tree is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_subtree;
+	int right_subtree;
+
+	if (!tree)
+		return (0);
+
+	left_subtree = binary_tree_height(tree->left);
+	right_subtree = binary_tree_height(tree->right);
+	return (left_subtree - right_subtree);
 }
 
 /**
